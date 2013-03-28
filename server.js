01a4d7af8fb1a7bc239c3c250d6c8da771f0162e
micha <trieba@e-gestaltung.de>
2013-03-28 07:50:53 +0100

    mongo basicaly integrated

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
     technologies = require('./data/technologies');
-
 var app = express();
  
-app.get('/technologies', technologies.findAll);
-app.get('/technologies/:id', technologies.findById);
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+ 
+app.get('/wines', technologies.findAll);
+app.get('/wines/:id', technologies.findById);
+app.post('/wines', technologies.addWine);
+app.put('/wines/:id', technologies.updateWine);
+app.delete('/wines/:id', technologies.deleteWine);
  
 app.listen(3000);
 console.log('Listening on port 3000...');
